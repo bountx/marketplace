4a7f28a2713a738624b2687fcb9ea7f9ab902a8a
bountx <andrzej.pijanowski@gmail.com>
2024-06-03 15:11:34 +0200

    added navbar with working logout button

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import { Turbo } from "@hotwired/turbo-rails"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+Turbo.start()
