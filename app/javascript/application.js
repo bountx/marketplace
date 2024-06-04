9702a0332e489a76d161d98678d3734c0b12c9af
bountx <andrzej.pijanowski@gmail.com>
2024-06-04 22:14:41 +0200

    carousel fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,11 @@
-//= require_tree .
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
